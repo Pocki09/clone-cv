aa52f6134c314b041047ec012edbc08a1dff1241
Pocki09 <mha5042109@gmail.com>
2025-04-02 13:37:21 +0700

    Đẩy project Clone CV lên GitHub

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1216px',
+      '2xl': '1216px',
+    },
+    extend: {
+      colors: {
+        
+      }
+    }
+  }
+}
